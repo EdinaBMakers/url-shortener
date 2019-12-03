0f8a74fac679d5167f2de51f1099e2c2636761fb
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-12-03 14:57:28 +0000

    Reuse short url for same original url

--- app.rb
+++ app.rb
@@ -4,11 +4,17 @@ require './lib/short_url_generator'
 
 class UrlShortener < Sinatra::Base
   short_url_generator = ShortUrlGenerator.new
+  short_url_by_original_url = {}
 
   post '/' do
     original_url = JSON.parse(request.body.read)["url"]
-    short_url = short_url_generator.get_short_url
+    short_url = short_url_by_original_url[original_url]
 
+    if short_url == nil
+      short_url = short_url_generator.get_short_url
+      short_url_by_original_url[original_url] = short_url
+    end
+    
     JSON[{"short_url" => "/#{short_url}", "url" => original_url}]
   end
 
