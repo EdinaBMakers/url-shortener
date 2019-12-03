5733c9c6bb7d6397bcd585c5b75a4fa08a06db20
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-12-03 15:27:25 +0000

    Respond with same short url for url with or without http://

--- app.rb
+++ app.rb
@@ -9,11 +9,12 @@ class UrlShortener < Sinatra::Base
 
   post '/' do
     original_url = JSON.parse(request.body.read)["url"]
-    short_url = short_url_by_original_url[original_url]
+    original_url_without_http = original_url.delete_prefix('http://')
+    short_url = short_url_by_original_url[original_url_without_http]
 
     if short_url == nil
       short_url = short_url_generator.get_short_url
-      short_url_by_original_url[original_url] = short_url
+      short_url_by_original_url[original_url_without_http] = short_url
       original_url_by_short_url[short_url] = original_url
     end
     
