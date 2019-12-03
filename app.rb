e08a0f71ddf46fb8b7d154e4ac55c2659b3509c8
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-12-03 14:48:30 +0000

    Generate short url and return short and original

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 require 'sinatra/base'
+require 'json'
 require './lib/short_url_generator'
 
 class UrlShortener < Sinatra::Base
+  short_url_generator = ShortUrlGenerator.new
+
   post '/' do
-    
+    original_url = JSON.parse(request.body.read)["url"]
+    short_url = short_url_generator.get_short_url
+
+    JSON[{"short_url" => "/#{short_url}", "url" => original_url}]
   end
 
   get '/:short_url' do
-    
+
   end
 end
