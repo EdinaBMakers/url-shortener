7f9ec4a7aec5cf4a8125bcb7251a3c92ef0c0ec3
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-12-03 13:38:12 +0000

    Return unique url for each call

--- lib/short_url_generator.rb
+++ lib/short_url_generator.rb
@@ -1,5 +1,9 @@
 class ShortUrlGenerator
+  def initialize
+    @valid_chars = ['a'..'z', 'A'..'Z', '0'..'9'].flat_map(&:to_a) 
+  end
+
   def get_short_url
-    "a"
+    @valid_chars.shift
   end
 end
--- spec/short_url_generator_spec.rb
+++ spec/short_url_generator_spec.rb
@@ -6,4 +6,11 @@ describe 'ShortUrlGenerator' do
 
     expect(subject.get_short_url).to eq('a')
   end
+
+  it 'returns with the next short url when called multiple times' do
+    subject = ShortUrlGenerator.new
+
+    expect(subject.get_short_url).to eq('a')
+    expect(subject.get_short_url).to eq('b')
+  end
 end
